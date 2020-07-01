9578002fe97c0b41bdb7e4e9f0b7b43cc173f5ba
Julian Andrej <andrej1@llnl.gov>
2020-07-01 14:08:19 -0700

    add petsc mg variation

--- examples/petsc/ex1p.cpp
+++ examples/petsc/ex1p.cpp
@@ -1,33 +1,5 @@
-//                       MFEM Example 1 - Parallel Version
-//                              PETSc Modification
-//
-// Compile with: make ex1p
-//
-// Sample runs:
-//    mpirun -np 4 ex1p -m ../../data/amr-quad.mesh
-//    mpirun -np 4 ex1p -m ../../data/amr-quad.mesh --petscopts rc_ex1p
-//
-// Description:  This example code demonstrates the use of MFEM to define a
-//               simple finite element discretization of the Laplace problem
-//               -Delta u = 1 with homogeneous Dirichlet boundary conditions.
-//               Specifically, we discretize using a FE space of the specified
-//               order, or if order < 1 using an isoparametric/isogeometric
-//               space (i.e. quadratic for quadratic curvilinear mesh, NURBS for
-//               NURBS mesh, etc.)
-//
-//               The example highlights the use of mesh refinement, finite
-//               element grid functions, as well as linear and bilinear forms
-//               corresponding to the left-hand side and right-hand side of the
-//               discrete linear system. We also cover the explicit elimination
-//               of essential boundary conditions, static condensation, and the
-//               optional connection to the GLVis tool for visualization.
-//               The example also shows how PETSc Krylov solvers can be used by
-//               wrapping a HypreParMatrix (or not) and a Solver, together with
-//               customization using an options file (see rc_ex1p) We also
-//               provide an example on how to visualize the iterative solution
-//               inside a PETSc solver.
-
 #include "mfem.hpp"
+#include "petsc.h"
 #include <fstream>
 #include <iostream>
 
@@ -38,281 +10,263 @@
 using namespace std;
 using namespace mfem;
 
-class UserMonitor : public PetscSolverMonitor
+class LevelContext
 {
-private:
-   ParBilinearForm *_a;
-   ParLinearForm *_b;
-
 public:
-   UserMonitor(ParBilinearForm *a, ParLinearForm *b)
-      : PetscSolverMonitor(true,false), _a(a), _b(b) {}
-
-   void MonitorSolution(PetscInt it, PetscReal norm, const Vector &X)
+   LevelContext(ParMesh *mesh, FiniteElementCollection *fec)
+      : pmesh(*mesh), pfes(new ParFiniteElementSpace(mesh, fec)), one(1.0)
    {
-      // we plot the first 5 iterates
-      if (!it || it > 5) { return; }
-      ParFiniteElementSpace *fespace = _a->ParFESpace();
-      ParMesh *mesh = fespace->GetParMesh();
-      ParGridFunction _x(fespace);
-      _a->RecoverFEMSolution(X, *_b, _x);
+      Xgf = new ParGridFunction(pfes);
 
-      char vishost[] = "localhost";
-      int  visport   = 19916;
-      int  num_procs, myid;
+      Xwork = new Vector(pfes->TrueVSize());
+      *Xwork = 0.0;
 
-      MPI_Comm_size(mesh->GetComm(),&num_procs);
-      MPI_Comm_rank(mesh->GetComm(),&myid);
-      socketstream sol_sock(vishost, visport);
-      sol_sock << "parallel " << num_procs << " " << myid << "\n";
-      sol_sock.precision(8);
-      sol_sock << "solution\n" << *mesh << _x
-               << "window_title 'Iteration no " << it << "'" << flush;
-   }
+      Array<int> ess_bdr(pmesh.bdr_attributes.Max());
+      ess_bdr = 1;
+
+      bform = new ParLinearForm(pfes);
+      bform->AddDomainIntegrator(new DomainLFIntegrator(one));
+      bform->Assemble();
+
+      aform = new ParBilinearForm(pfes);
+      aform->AddDomainIntegrator(new DiffusionIntegrator(one));
+      aform->Assemble();
+      aform->EliminateEssentialBC(ess_bdr, *Xgf, *bform);
+      aform->Finalize();
+
+      H.SetType(Operator::PETSC_MATAIJ);
+      aform->ParallelAssemble(H);
+
+      Xwork = Xgf->ParallelAssemble();
+      Rwork = bform->ParallelAssemble();
+
+      Xwork_petsc = new PetscParVector(MPI_COMM_WORLD, *Xwork, true);
+      Rwork_petsc = new PetscParVector(MPI_COMM_WORLD, *Rwork, true);
+
+      T.SetType(Operator::Hypre_ParCSR);
+   };
+
+   ParMesh pmesh;
+   ParFiniteElementSpace *pfes = nullptr;
+   Vector *Xwork = nullptr;
+   PetscParVector *Xwork_petsc = nullptr;
+   OperatorHandle T;
+   Mat PRshell;
+   Vector *Rwork = nullptr;
+   PetscParVector *Rwork_petsc = nullptr;
+   OperatorHandle H;
+   ParBilinearForm *aform = nullptr;
+   ParLinearForm *bform = nullptr;
+   ParGridFunction *Xgf = nullptr;
+   ConstantCoefficient one;
 };
+
+static PetscErrorCode MatMult_Prolong(Mat A, Vec X, Vec Y)
+{
+   PetscErrorCode ierr;
+   LevelContext *ctx;
+   PetscScalar *c, *f;
+
+   PetscFunctionBeginUser;
+
+   ierr = MatShellGetContext(A, &ctx);
+   CHKERRQ(ierr);
+
+   PetscParVector X_wrap(X, true);
+   PetscParVector Y_wrap(Y, true);
+
+   PetscPrintf(MPI_COMM_WORLD, "Prolong MATSHELL: %p\n", (void *) A);
+   ctx->T->Mult(X_wrap, Y_wrap);
+
+   PetscFunctionReturn(0);
+}
+
+static PetscErrorCode MatMult_Restrict(Mat A, Vec X, Vec Y)
+{
+   PetscErrorCode ierr;
+   LevelContext *ctx;
+   PetscScalar *c, *f;
+
+   PetscFunctionBeginUser;
+
+   ierr = MatShellGetContext(A, &ctx);
+   CHKERRQ(ierr);
+
+   PetscParVector X_wrap(X, true);
+   PetscParVector Y_wrap(Y, true);
+
+   PetscPrintf(MPI_COMM_WORLD, "Restrict MATSHELL: %p\n", (void *) A);
+   ctx->T->MultTranspose(X_wrap, Y_wrap);
+
+   PetscFunctionReturn(0);
+}
 
 int main(int argc, char *argv[])
 {
-   // 1. Initialize MPI.
    int num_procs, myid;
    MPI_Init(&argc, &argv);
    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
-   // 2. Parse command-line options.
-   const char *mesh_file = "../../data/star.mesh";
-   int order = 1;
+   const char *mesh_file = "../data/inline-quad.mesh";
+   int order = 2;
    bool static_cond = false;
    bool visualization = false;
    bool use_petsc = true;
    const char *petscrc_file = "";
    bool petscmonitor = false;
 
-   OptionsParser args(argc, argv);
-   args.AddOption(&mesh_file, "-m", "--mesh",
-                  "Mesh file to use.");
-   args.AddOption(&order, "-o", "--order",
-                  "Finite element order (polynomial degree) or -1 for"
-                  " isoparametric space.");
-   args.AddOption(&static_cond, "-sc", "--static-condensation", "-no-sc",
-                  "--no-static-condensation", "Enable static condensation.");
-   args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
-                  "--no-visualization",
-                  "Enable or disable GLVis visualization.");
-   args.AddOption(&use_petsc, "-usepetsc", "--usepetsc", "-no-petsc",
-                  "--no-petsc",
-                  "Use or not PETSc to solve the linear system.");
-   args.AddOption(&petscrc_file, "-petscopts", "--petscopts",
-                  "PetscOptions file to use.");
-   args.AddOption(&petscmonitor, "-petscmonitor", "--petscmonitor",
-                  "-no-petscmonitor", "--no-petscmonitor",
-                  "Enable or disable GLVis visualization of residual.");
-   args.Parse();
-   if (!args.Good())
-   {
-      if (myid == 0)
-      {
-         args.PrintUsage(cout);
-      }
-      MPI_Finalize();
-      return 1;
-   }
-   if (myid == 0)
-   {
-      args.PrintOptions(cout);
-   }
+   MFEMInitializePetsc(&argc, &argv, petscrc_file, NULL);
 
-   // 2b. We initialize PETSc
-   MFEMInitializePetsc(NULL,NULL,petscrc_file,NULL);
-
-   // 3. Read the (serial) mesh from the given mesh file on all processors.  We
-   //    can handle triangular, quadrilateral, tetrahedral, hexahedral, surface
-   //    and volume meshes with the same code.
    Mesh *mesh = new Mesh(mesh_file, 1, 1);
    int dim = mesh->Dimension();
 
-   // 4. Refine the serial mesh on all processors to increase the resolution. In
-   //    this example we do 'ref_levels' of uniform refinement. We choose
-   //    'ref_levels' to be the largest number that gives a final mesh with no
-   //    more than 10,000 elements.
+   for (int i = 0; i < 4; i++)
    {
-      int ref_levels =
-         (int)floor(log(10000./mesh->GetNE())/log(2.)/dim);
-      for (int l = 0; l < ref_levels; l++)
-      {
-         mesh->UniformRefinement();
-      }
+      mesh->UniformRefinement();
    }
 
-   // 5. Define a parallel mesh by a partitioning of the serial mesh. Refine
-   //    this mesh further in parallel to increase the resolution. Once the
-   //    parallel mesh is defined, the serial mesh can be deleted.
    ParMesh *pmesh = new ParMesh(MPI_COMM_WORLD, *mesh);
    delete mesh;
+
+   FiniteElementCollection *fec = new H1_FECollection(order, dim);
+
+   // Number of MG levels including finest, level 0 is coarsest
+   int numlevels = 3;
+   int ierr;
+
+   Array<LevelContext *> levelctx(numlevels);
+
+   for (int i = 0; i < numlevels; i++)
    {
-      int par_ref_levels = 2;
-      for (int l = 0; l < par_ref_levels; l++)
+      levelctx[i] = new LevelContext(pmesh, fec);
+
+      PetscPrintf(MPI_COMM_WORLD,
+                  "Adding FE space of size %d\n",
+                  levelctx[i]->pfes->GlobalTrueVSize());
+
+      PetscPrintf(MPI_COMM_WORLD,
+                  "%d MATAIJ %p\n",
+                  i,
+                  (void *) levelctx[i]->H.As<PetscParMatrix>());
+
+      if (i > 0)
+      {
+         levelctx[i]->pfes->GetTrueTransferOperator(*levelctx[i - 1]->pfes,
+                                                    levelctx[i]->T);
+
+         levelctx[i]->PRshell = *(new Mat);
+
+         ierr = MatCreateShell(MPI_COMM_WORLD,
+                               levelctx[i]->pfes->GetTrueVSize(),
+                               levelctx[i - 1]->pfes->GetTrueVSize(),
+                               levelctx[i]->pfes->GlobalTrueVSize(),
+                               levelctx[i - 1]->pfes->GlobalTrueVSize(),
+                               &*levelctx[i],
+                               &levelctx[i]->PRshell);
+         CHKERRQ(ierr);
+
+         LevelContext *ctx;
+         ierr = MatShellGetContext(levelctx[i]->PRshell, &ctx);
+         CHKERRQ(ierr);
+
+         PetscPrintf(MPI_COMM_WORLD,
+                     "%d MATSHELL %p\n",
+                     i,
+                     (void *) levelctx[i]->PRshell);
+
+         ierr = MatShellSetOperation(levelctx[i]->PRshell,
+                                     MATOP_MULT,
+                                     (void (*)(void)) MatMult_Prolong);
+         CHKERRQ(ierr);
+
+         ierr = MatShellSetOperation(levelctx[i]->PRshell,
+                                     MATOP_MULT_TRANSPOSE,
+                                     (void (*)(void)) MatMult_Restrict);
+         CHKERRQ(ierr);
+      }
+
+      if (i < numlevels - 1)
       {
          pmesh->UniformRefinement();
       }
    }
 
-   // 6. Define a parallel finite element space on the parallel mesh. Here we
-   //    use continuous Lagrange finite elements of the specified order. If
-   //    order < 1, we instead use an isoparametric/isogeometric space.
-   FiniteElementCollection *fec;
-   if (order > 0)
+   KSP ksp;
+   ierr = KSPCreate(MPI_COMM_WORLD, &ksp);
+   CHKERRQ(ierr);
+   ierr = KSPSetType(ksp, KSPCG);
+   CHKERRQ(ierr);
+   ierr = KSPSetNormType(ksp, KSP_NORM_NATURAL);
+   CHKERRQ(ierr);
+   ierr = KSPSetFromOptions(ksp);
+   CHKERRQ(ierr);
+
+   ierr = KSPSetOperators(ksp,
+                          *levelctx.Last()->H.As<PetscParMatrix>(),
+                          *levelctx.Last()->H.As<PetscParMatrix>());
+   CHKERRQ(ierr);
+
+   PC pc;
+   ierr = KSPGetPC(ksp, &pc);
+   CHKERRQ(ierr);
+
+   ierr = PCSetType(pc, PCMG);
+   CHKERRQ(ierr);
+
+   ierr = PCMGSetLevels(pc, numlevels, nullptr);
+   CHKERRQ(ierr);
+
+   for (int i = 0; i < numlevels; i++)
    {
-      fec = new H1_FECollection(order, dim);
-   }
-   else if (pmesh->GetNodes())
-   {
-      fec = pmesh->GetNodes()->OwnFEC();
-      if (myid == 0)
+      KSP smoother;
+      PC smoother_pc;
+      ierr = PCMGGetSmoother(pc, i, &smoother);
+      CHKERRQ(ierr);
+      ierr = KSPSetOperators(smoother,
+                             *levelctx[i]->H.As<PetscParMatrix>(),
+                             *levelctx[i]->H.As<PetscParMatrix>());
+      CHKERRQ(ierr);
+
+      if (i < numlevels - 1)
       {
-         cout << "Using isoparametric FEs: " << fec->Name() << endl;
+         ierr = PCMGSetX(pc, i, *levelctx[i]->Xwork_petsc);
+         CHKERRQ(ierr);
+      }
+
+      if (i > 0)
+      {
+         ierr = PCMGSetInterpolation(pc, i, levelctx[i]->PRshell);
+         CHKERRQ(ierr);
       }
    }
-   else
-   {
-      fec = new H1_FECollection(order = 1, dim);
-   }
-   ParFiniteElementSpace *fespace = new ParFiniteElementSpace(pmesh, fec);
-   HYPRE_Int size = fespace->GlobalTrueVSize();
-   if (myid == 0)
-   {
-      cout << "Number of finite element unknowns: " << size << endl;
-   }
 
-   // 7. Determine the list of true (i.e. parallel conforming) essential
-   //    boundary dofs. In this example, the boundary conditions are defined
-   //    by marking all the boundary attributes from the mesh as essential
-   //    (Dirichlet) and converting them to a list of true dofs.
-   Array<int> ess_tdof_list;
-   if (pmesh->bdr_attributes.Size())
-   {
-      Array<int> ess_bdr(pmesh->bdr_attributes.Max());
-      ess_bdr = 1;
-      fespace->GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
-   }
+   ierr = PCMGSetType(pc, PC_MG_MULTIPLICATIVE);
+   CHKERRQ(ierr);
 
-   // 8. Set up the parallel linear form b(.) which corresponds to the
-   //    right-hand side of the FEM linear system, which in this case is
-   //    (1,phi_i) where phi_i are the basis functions in fespace.
-   ParLinearForm *b = new ParLinearForm(fespace);
-   ConstantCoefficient one(1.0);
-   b->AddDomainIntegrator(new DomainLFIntegrator(one));
-   b->Assemble();
+   ierr = KSPSolve(ksp,
+                   *levelctx.Last()->Rwork_petsc,
+                   *levelctx.Last()->Xwork_petsc);
+   CHKERRQ(ierr);
 
-   // 9. Define the solution vector x as a parallel finite element grid function
-   //    corresponding to fespace. Initialize x with initial guess of zero,
-   //    which satisfies the boundary conditions.
-   ParGridFunction x(fespace);
-   x = 0.0;
+   levelctx.Last()->Xgf->SetFromTrueDofs(*levelctx.Last()->Xwork_petsc);
 
-   // 10. Set up the parallel bilinear form a(.,.) on the finite element space
-   //     corresponding to the Laplacian operator -Delta, by adding the Diffusion
-   //     domain integrator.
-   ParBilinearForm *a = new ParBilinearForm(fespace);
-   a->AddDomainIntegrator(new DiffusionIntegrator(one));
-
-   // 11. Assemble the parallel bilinear form and the corresponding linear
-   //     system, applying any necessary transformations such as: parallel
-   //     assembly, eliminating boundary conditions, applying conforming
-   //     constraints for non-conforming AMR, static condensation, etc.
-   if (static_cond) { a->EnableStaticCondensation(); }
-   a->Assemble();
-
-   HypreParMatrix A;
-   Vector B, X;
-   a->FormLinearSystem(ess_tdof_list, x, *b, A, X, B);
-
-   if (myid == 0)
-   {
-      cout << "Size of linear system: " << A.GetGlobalNumRows() << endl;
-   }
-
-   // 12. Define and apply a parallel PCG solver for AX=B with the BoomerAMG
-   //     preconditioner from hypre.
-   HypreSolver *amg = new HypreBoomerAMG(A);
-
-   if (!use_petsc)
-   {
-      HyprePCG *pcg = new HyprePCG(A);
-      pcg->SetPreconditioner(*amg);
-      pcg->SetTol(1e-12);
-      pcg->SetMaxIter(200);
-      pcg->SetPrintLevel(2);
-      pcg->Mult(B, X);
-      delete pcg;
-   }
-   else
-   {
-      // If petscrc_file has been given, we convert the HypreParMatrix to a
-      // PetscParMatrix; the user can then experiment with PETSc command line
-      // options.
-      bool wrap = !strlen(petscrc_file);
-      PetscPCGSolver *pcg = new PetscPCGSolver(A, wrap);
-      if (wrap)
-      {
-         pcg->SetPreconditioner(*amg);
-      }
-      pcg->SetTol(1e-12);
-      pcg->SetAbsTol(1e-12);
-      pcg->SetMaxIter(200);
-      pcg->SetPrintLevel(2);
-
-      UserMonitor mymon(a,b);
-      if (visualization && petscmonitor)
-      {
-         pcg->SetMonitor(&mymon);
-         pcg->iterative_mode = true;
-         X.Randomize();
-      }
-      pcg->Mult(B, X);
-      delete pcg;
-   }
-
-   // 13. Recover the parallel grid function corresponding to X. This is the
-   //     local finite element solution on each processor.
-   a->RecoverFEMSolution(X, *b, x);
-
-   // 14. Save the refined mesh and the solution in parallel. This output can
-   //     be viewed later using GLVis: "glvis -np <np> -m mesh -g sol".
-   {
-      ostringstream mesh_name, sol_name;
-      mesh_name << "mesh." << setfill('0') << setw(6) << myid;
-      sol_name << "sol." << setfill('0') << setw(6) << myid;
-
-      ofstream mesh_ofs(mesh_name.str().c_str());
-      mesh_ofs.precision(8);
-      pmesh->Print(mesh_ofs);
-
-      ofstream sol_ofs(sol_name.str().c_str());
-      sol_ofs.precision(8);
-      x.Save(sol_ofs);
-   }
-
-   // 15. Send the solution by socket to a GLVis server.
-   if (visualization)
+   if (true)
    {
       char vishost[] = "localhost";
-      int  visport   = 19916;
+      int visport = 19916;
       socketstream sol_sock(vishost, visport);
       sol_sock << "parallel " << num_procs << " " << myid << "\n";
       sol_sock.precision(8);
-      sol_sock << "solution\n" << *pmesh << x << flush;
+      sol_sock << "solution\n"
+               << levelctx.Last()->pmesh << *levelctx.Last()->Xgf << flush;
    }
 
-   // 16. Free the used memory.
-   delete amg;
-   delete a;
-   delete b;
-   delete fespace;
-   if (order > 0) { delete fec; }
-   delete pmesh;
+   // delete a;
+   // delete b;
+   // delete fec;
+   // delete pmesh;
 
-   // We finalize PETSc
    MFEMFinalizePetsc();
 
    MPI_Finalize();
